cd0cb4e963ecbb97551a1fbc67140aeec43991cf
Glory42 <gorkemkaryol03@gmail.com>
2025-10-10 22:24:58 +0300

    feat: add dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import 'dotenv/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    define: {
+      'import.meta.env.GITHUB_TOKEN': JSON.stringify(process.env.GITHUB_TOKEN),
+    }
   }
 });
