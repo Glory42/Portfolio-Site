551cf176b77df14e45769b662fa6261189879d91
Glory42 <gorkemkaryol03@gmail.com>
2025-10-16 22:42:38 +0300

    revert

--- src/scripts/fetchers/GithubProjectFetcher.ts
+++ src/scripts/fetchers/GithubProjectFetcher.ts
@@ -1,5 +1,18 @@
-export async function fetchGithubProject() {
-  const res = await fetch('https://ma-portfolio-backend.onrender.com/api/github');
-  if (!res.ok) throw new Error('Backend fetch failed');
-  return await res.json();
-}
+import type { Project } from "../../types/Project.ts";
+
+export async function fetchGithubProject(): Promise<Project[]> {
+  try {
+    const res = await fetch('https://ma-portfolio-backend.onrender.com/api/github', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (!res.ok) throw new Error(`Backend fetch failed: ${res.status}`);
+
+    const projects: Project[] = await res.json();
+    return projects;
+  } catch (err) {
+    console.error('error fetching projects: ', err);
+    return [];
+  }
+};
