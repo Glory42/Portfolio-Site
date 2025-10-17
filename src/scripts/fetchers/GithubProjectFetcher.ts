d4e77966543dc27cf765f689f6c0b48c775926ba
Glory42 <gorkemkaryol03@gmail.com>
2025-10-17 15:20:40 +0300

    refactor: better readabily.

--- src/scripts/fetchers/GithubProjectFetcher.ts
+++ src/scripts/fetchers/GithubProjectFetcher.ts
@@ -16,7 +16,8 @@ export async function fetchGithubProjects(): Promise<void> {
 
     const projects = await response.json();
     projectsContainer.innerHTML = projects.map((p: any) => `
-      <div class="w-80 min-h-[220px] border-2 border-gray-700 rounded-3xl p-4 flex flex-col justify-between hover:bg-gray-900 transition-transform hover:-translate-y-0.5 shadow-[0_2px_5px_rgba(0,0,0,0.3)]">
+      <div class="w-80 min-h-[220px] border-2 border-gray-700 rounded-3xl p-4 flex flex-col 
+      justify-between hover:bg-gray-900 transition-transform hover:-translate-y-0.5 shadow-[0_2px_5px_rgba(0,0,0,0.3)]">
         <h3 class="text-lg font-semibold">${p.name}</h3>
         <p class="text-gray-400">${p.description}</p>
         <a href="${p.url}" target="_blank" class="text-amber-200 hover:text-amber-400">View on GitHub</a>
